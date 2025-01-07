9ea5117bd659e81c3b372bc4616ac98f30a3e734
Lyezus <111443087+Lyezus@users.noreply.github.com>
2025-01-07 12:17:04 +0100

    Update GunMeregeDetector.cs

--- Assets/Scripts/GunMeregeDetector.cs
+++ Assets/Scripts/GunMeregeDetector.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class GunMeregeDetector : MonoBehaviour
 {
-    [SerializeField] AutoShoot _gunFront;
-    [SerializeField] AutoShoot _gunBack;
-    // Start is called before the first frame update
+    [SerializeField] GameObject _gunFront;
+    [SerializeField] GameObject _gunFrontVisual;
+
+    [SerializeField] GameObject _gunBack;
+
+    [SerializeField] GameObject _mergedGun;
+    [SerializeField] GameObject _mergedGunVisual;
+
+    AutoShoot shootFront;
+    AutoShoot shootBack;
+    private void Start()
+    {
+        _gunFront.TryGetComponent<AutoShoot>(out shootFront);
+        _gunBack.TryGetComponent<AutoShoot>(out shootBack);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Gun"))
         {
-            _gunBack.enabled = false;
-            _gunFront.enabled = false;
+            shootFront.enabled = false;
+            shootBack.enabled = false;
+
+            _gunFrontVisual.SetActive(false);
+            _gunBack.SetActive(false);
+            _mergedGun.SetActive(true);
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Gun")) 
         {
-            _gunFront.enabled = true;
-            _gunBack.enabled = true;
+            _gunFrontVisual.SetActive(true);
+            _gunBack.SetActive(true);
+            _mergedGun.SetActive(false);
+
+            shootFront.enabled = true;
+            shootBack.enabled = true;
         }
     }
   
