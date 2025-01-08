5ca64609179b75adb18dc00d8a966084a1e3601f
Lyezus <111443087+Lyezus@users.noreply.github.com>
2025-01-08 16:29:29 +0100

    fixed start button

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] TMPro.TextMeshPro _infoText;
+    [SerializeField] TMP_Text _infoText;
 
     // Start is called before the first frame update
     void Start()
